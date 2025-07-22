cb25f7f5edc8fc7bb5cd9d583ec04d8a55ccc5f3
Yujin Jung <devyuu08@gmail.com>
2025-07-22 10:53:16 +0900

    feat: 계산 결과 페이지 기본 구조 및 에러 카드 UI 구현

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,15 +1,36 @@
+"use client";
+
+import { useTaxContext } from "@/context/TaxContext";
 import ResultCard from "@/components/result/ResultCard";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Page() {
+  const { result } = useTaxContext();
+
+  if (!result) {
+    return (
+      <div className={styles.errorCard}>
+        <h2>앗, 이런!</h2>
+        <p>
+          결과를 불러오는 데 문제가 발생했어요. <br />
+          홈으로 돌아가 다시 시도해 주세요.
+        </p>
+        <Link href="/" className={styles.backButton}>
+          홈으로 돌아가기
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <p className={styles.badge}>계산 완료</p>
       <h1 className={styles.title}>세금 계산 결과</h1>
       <p className={styles.sub}>
-        (사용자의 입력값)원 소득 기준으로 예상한 세금 결과입니다.
+        {result.income.toLocaleString()}원 소득 기준으로 예상한 세금 결과입니다.
       </p>
-      <ResultCard />
+      <ResultCard result={result} />
     </div>
   );
 }
